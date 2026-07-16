1b3de664866a685214828e564f6c4971c7618f6a
aarushii-create <aarushiiiii.sharmaaa@gmail.com>
2026-07-16 11:43:51 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.length(), m=t.length();
+        if(n!=m) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s==t) return true;
+        return false;
+    }
+};
